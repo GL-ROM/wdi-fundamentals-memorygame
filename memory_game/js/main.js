61e38578f07177b1058e7e33dd19726385a72a03
GLR-901 <gabriel.l.romero@gmail.com>
2020-02-04 13:15:42 -0500

    Game logic and function updates

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,21 @@ console.log("Up and Running! Oh Yeah!");
 const cards = ["Queen", "Queen", "King", "King"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[2]) {
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match! Congrats!");
     } else {
       alert("Sorry, try again!");
     }
 }
+
+function flipCard(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2) {
+  checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
