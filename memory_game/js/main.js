aab5ecb998ddcc788ab8116f6a1f19da16610342
GLR-901 <gabriel.l.romero@gmail.com>
2020-02-04 14:10:39 -0500

    Added Objects for cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and Running! Oh Yeah!");
 
-const cards = ["Queen", "Queen", "King", "King"];
+const cards = [
+  {
+    rank: 'Queen',
+    suit: 'Hearts',
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: 'Queen',
+    suit: 'Diamonds',
+    cardImage: "images/queen-of-diamons.png"
+  },
+  {
+    rank: 'King',
+    suit: 'Hearts',
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: 'King',
+    suit: 'Diamonds',
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+
 const cardsInPlay = [];
 
 function checkForMatch(){
@@ -12,8 +34,10 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   if (cardsInPlay.length === 2) {
   checkForMatch();
   }
