5fc8ed583508e9baea0ceb596fa30981f09fc719
GLR-901 <gabriel.l.romero@gmail.com>
2020-02-04 16:37:22 -0500

    Working Games Sans Randomizer

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,7 +23,7 @@ const cards = [
   }
 ];
 
-const cardsInPlay = [];
+var cardsInPlay = [];
 
 function checkForMatch(){
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -33,15 +33,24 @@ function checkForMatch(){
     }
 }
 
-function flipCard(cardId){
-  console.log("User flipped " + cards[cardId].rank);
+function flipCard(){
+  var cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
   checkForMatch();
   }
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+
+  for(let i = 0; i < cards.length; i++){
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src','images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+createBoard();
